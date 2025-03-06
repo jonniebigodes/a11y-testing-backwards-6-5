8f49a8f077c784e0ae7d7743b34a7a7bb3fa9e0d
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-03-06 15:42:05 +0000

    Initial commit

---
+++ src/components/InboxScreen.stories.js
@@ -0,0 +1,128 @@
+// Initial implementation of the InboxScreen.stories.js file
+
+// import React from "react";
+
+// import InboxScreen from "./InboxScreen";
+// import store from "../lib/store";
+
+// import { Provider } from "react-redux";
+
+// export default {
+//   component: InboxScreen,
+//   title: "InboxScreen",
+//   decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+// };
+
+// const Template = () => <InboxScreen />;
+
+// export const Default = Template.bind({});
+// export const Error = Template.bind({});
+
+// Updated InboxScreen.stories.js file for the Screen section
+// import React from "react";
+
+// import InboxScreen from "./InboxScreen";
+// import store from "../lib/store";
+// import { rest } from "msw";
+// import { MockedState } from "./TaskList.stories";
+// import { Provider } from "react-redux";
+
+// export default {
+//   component: InboxScreen,
+//   title: "InboxScreen",
+//   decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+// };
+
+// const Template = () => <InboxScreen />;
+
+// export const Default = Template.bind({});
+// Default.parameters = {
+//   msw: {
+//     handlers: [
+//       rest.get(
+//         "https://jsonplaceholder.typicode.com/todos?userId=1",
+//         (req, res, ctx) => {
+//           return res(ctx.json(MockedState.tasks));
+//         }
+//       ),
+//     ],
+//   },
+// };
+
+// export const Error = Template.bind({});
+// Error.parameters = {
+//   msw: {
+//     handlers: [
+//       rest.get(
+//         "https://jsonplaceholder.typicode.com/todos?userId=1",
+//         (req, res, ctx) => {
+//           return res(ctx.status(403));
+//         }
+//       ),
+//     ],
+//   },
+// };
+
+// Updated InboxScreen.stories.js file to include the Integration testing section
+import React from "react";
+
+import InboxScreen from "./InboxScreen";
+import store from "../lib/store";
+import { rest } from "msw";
+import { MockedState } from "./TaskList.stories";
+import { Provider } from "react-redux";
+import {
+  fireEvent,
+  within,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@storybook/testing-library";
+
+export default {
+  component: InboxScreen,
+  title: "InboxScreen",
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+};
+
+const Template = () => <InboxScreen />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      ),
+    ],
+  },
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByTestId("pinTask-1"));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByTestId("pinTask-3"));
+  });
+};
+
+export const Error = Template.bind({});
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=1",
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
