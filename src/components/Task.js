3456e447dcf7a698f1fe6eb867ba478ecd065f30
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2025-03-06 16:10:32 +0000

    Code cleanup

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,3 @@
-/* import React from "react";
-
-export default function Task({
-  task: { id, title, state },
-  onArchiveTask,
-  onPinTask,
-}) {
-  return (
-    <div className="list-item">
-      <label>
-        <input type="text" value={title} readOnly={true} name="title" />
-      </label>
-    </div>
-  );
-}
- */
-
-// Updated task component without a11y
-
 import React from "react";
 
 import PropTypes from "prop-types";
